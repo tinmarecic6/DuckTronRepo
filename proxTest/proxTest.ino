bc6483b5eee4f02458461b7798cdf3f7a602603c
DjStonie <valdonay@yahoo.dk>
2022-05-17 11:26:06 +0200

    Update proxTest.ino

--- proxTest/proxTest.ino
+++ proxTest/proxTest.ino
@@ -1,9 +1,9 @@
 #include <Servo.h>
 
-int dir = 1;
+int dir = 1; // 1 = right, -1 = left with black towards power of the motor
 const int dirPin = 2;
 const int stepPin = 3;
-const int stepsPerRevolution = 205;
+const int stepsPerRevolution = 200;
 const int proximitySensorPin = 5;
 int proxVal;
 int servoPos = 0;
@@ -18,6 +18,7 @@ int topRightResVal;
 int topLeftResVal;
 int bottomRightResVal;
 int bottomLeftResVal;
+int maxRotation;
 Servo myservo; 
 
 
@@ -28,48 +29,51 @@ void setup() {
   pinMode(dirPin, OUTPUT); 
   pinMode(stepPin,OUTPUT);
   digitalWrite(dirPin,HIGH);
-  int i = 0;
+  // Test if the motor is already above the sensor, if so move it away and back to get the correct 0 position
+  if (digitalRead(proximitySensorPin) == 0){
+    debug = "Touched before moving, correting position";
+    digitalWrite(dirPin,LOW); //set direction and move it until it no longer touches the sensor
+    while(digitalRead(proximitySensorPin) == 0){
+      digitalWrite(stepPin,HIGH);
+      delay(5);
+      digitalWrite(stepPin,LOW);
+      delay(5);
+    }
+    for(int x = 0; x < 10; x++){ // move it 10 steps further to make sure its away from the sensor
+      digitalWrite(stepPin,HIGH);
+      delay(5);
+      digitalWrite(stepPin,LOW);
+      delay(5);
+    } // continue as if it never touched the sensor during setup
+  }
   while(zeroPosTouched == false){
-    digitalWrite(stepPin,HIGH);
-    delay(5);
-    digitalWrite(stepPin,LOW);
-    delay(5);
+    digitalWrite(dirPin,HIGH);
     proxVal = digitalRead(proximitySensorPin);
-    Serial.println(proxVal);
     if (proxVal == 0){
       zeroPosTouched = true;
       debug = "Touched";
     }
+    digitalWrite(stepPin,HIGH);
+    delay(5);
+    digitalWrite(stepPin,LOW);
+    delay(5);
     Serial.println(debug);
     delay(10);  
   }
+  Serial.println("Calibration done, waiting 5 seconds before starting");
   delay(5000);
-  digitalWrite(dirPin,LOW);
-  for(int x = 0; x < stepsPerRevolution; x++){
-    //stepPos += 1;
-    digitalWrite(stepPin, HIGH);
-    delay(5);
-    digitalWrite(stepPin, LOW);
-    delay(5);  
-  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  int val = digitalRead(proximitySensorPin);
-  
-  topRightResVal = analogRead(pRTopRightPin);
-  topLeftResVal = analogRead(pRTopLeftPin);
-  bottomRightResVal = analogRead(pRBottomRightPin);
-  bottomLeftResVal = analogRead(pRBottomLeftPin);
-  Serial.print("Top right:");
-  Serial.println(topRightResVal);
-  Serial.print("Top left:");
-  Serial.println(topLeftResVal);
-  Serial.print("Bottom right:");
-  Serial.println(bottomRightResVal);
-  Serial.print("Bottom left:");
-  Serial.println(bottomLeftResVal);
+  //Tins awesome decision code
+
   
   delay(1000);
+}
+
+void left() {
+  if (stepsPos > maxRotation){
+    
+  }
+  stepsPos += 1; // not sure if this is correct
 }
