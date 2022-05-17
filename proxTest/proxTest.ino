8e2a869aeb0ec65f011af2e34cd86e7657dfb2d6
DjStonie <valdonay@yahoo.dk>
2022-05-17 12:41:33 +0200

    Update proxTest.ino

--- proxTest/proxTest.ino
+++ proxTest/proxTest.ino
@@ -1,9 +1,8 @@
 #include <Servo.h>
 
-int dir = 1; // 1 = right, -1 = left with black towards power of the motor
 const int dirPin = 2;
 const int stepPin = 3;
-const int stepsPerRevolution = 200;
+const int maxRotation = 400; //stepsPerRevolution = 400 as we have a 1:2 gear ratio
 const int proximitySensorPin = 5;
 int proxVal;
 int servoPos = 0;
@@ -18,7 +17,6 @@ int topRightResVal;
 int topLeftResVal;
 int bottomRightResVal;
 int bottomLeftResVal;
-int maxRotation;
 int photoOffset = 3;
 Servo myservo;
 
@@ -30,10 +28,20 @@ void setup() {
   pinMode(dirPin, OUTPUT); 
   pinMode(stepPin,OUTPUT);
   digitalWrite(dirPin,HIGH);
+  Serial.println("Moving 50 steps to make sure the startup havent moved the motor too far");
+  for(int x = 0; x < 50; x++){
+      digitalWrite(dirPin,HIGH);// HIGH = left (counterclockwise), LOW = right (clockwise) with black towards power of the motor
+      digitalWrite(stepPin,HIGH);
+      delay(5);
+      digitalWrite(stepPin,LOW);
+      delay(5);
+  }
+  Serial.println("First steps done");
+  delay(2000);
   // Test if the motor is already above the sensor, if so move it away and back to get the correct 0 position
   if (digitalRead(proximitySensorPin) == 0){
-    debug = "Touched before moving, correting position";
-    digitalWrite(dirPin,LOW); //set direction and move it until it no longer touches the sensor
+    Serial.println("Touched before moving, correting position");
+    digitalWrite(dirPin,HIGH); //set direction and move it until it no longer touches the sensor
     while(digitalRead(proximitySensorPin) == 0){
       digitalWrite(stepPin,HIGH);
       delay(5);
@@ -47,30 +55,11 @@ void setup() {
       delay(5);
     } // continue as if it never touched the sensor during setup
   }
-  while(zeroPosTouched == false){
-    digitalWrite(dirPin,HIGH);
-    proxVal = digitalRead(proximitySensorPin);
-    if (proxVal == 0){
-      zeroPosTouched = true;
-      debug = "Touched";
-    }
-    digitalWrite(stepPin,HIGH);
-    delay(5);
-    digitalWrite(stepPin,LOW);
-    delay(5);
-    Serial.println(debug);
-    delay(10);
-  }
+  Serial.println("Touched before moving, correting position done");
+  delay(2000);
+  resetTo0();
   Serial.println("Calibration done, waiting 5 seconds before starting");
   delay(5000);
-  digitalWrite(dirPin, LOW);
-  for (int x = 0; x < stepsPerRevolution; x++) {
-    //stepPos += 1;
-    digitalWrite(stepPin, HIGH);
-    delay(5);
-    digitalWrite(stepPin, LOW);
-    delay(5);
-  }
 }
 
 void loop() {
@@ -81,39 +70,88 @@ void loop() {
   topLeftResVal = analogRead(pRTopLeftPin);
   bottomRightResVal = analogRead(pRBottomRightPin);
   bottomLeftResVal = analogRead(pRBottomLeftPin);
-  Serial.println("\n----------");
-  Serial.print("Top right:");
-  Serial.println(topRightResVal);
-  Serial.print("Top left:");
-  Serial.println(topLeftResVal);
-  Serial.print("Bottom right:");
-  Serial.println(bottomRightResVal);
-  Serial.print("Bottom left:");
-  Serial.println(bottomLeftResVal);
+  //Serial.println("\n----------");
+  //Serial.print("Top right:");
+  //Serial.println(topRightResVal);
+  //Serial.print("Top left:");
+  //Serial.println(topLeftResVal);
+  //Serial.print("Bottom right:");
+  //Serial.println(bottomRightResVal);
+  //Serial.print("Bottom left:");
+  //Serial.println(bottomLeftResVal);
   if (topLeftResVal > (topRightResVal + photoOffset) || bottomLeftResVal > (bottomRightResVal + photoOffset)) {
     //move to the left
     Serial.println("Moving to the left");
-  }
-  if (topLeftResVal > (topRightResVal + photoOffset) || bottomLeftResVal > (bottomRightResVal + photoOffset)) {
+    //left(5);
+  }else if (topLeftResVal > (topRightResVal + photoOffset) || bottomLeftResVal > (bottomRightResVal + photoOffset)) {
     //move to the right
     Serial.println("Moving to the right");
-  }
-  if (topLeftResVal > (bottomLeftResVal + photoOffset) || topRightResVal > (topRightResVal + photoOffset)) {
+    //right(5);
+  }else if (topLeftResVal > (bottomLeftResVal + photoOffset) || topRightResVal > (topRightResVal + photoOffset)) {
     //move up
     Serial.println("Moving up");
-  }
-  if (bottomLeftResVal > (topLeftResVal + photoOffset) || bottomRightResVal > (topRightResVal + photoOffset)) {
+  }else if (bottomLeftResVal > (topLeftResVal + photoOffset) || bottomRightResVal > (topRightResVal + photoOffset)) {
     //move down
     Serial.println("Moving down");
   }
 
-
-  delay(5000);
+  right(10);
+  delay(500);
 }
 
-void left() {
-  if (stepsPos > maxRotation){
-    
+void resetTo0(){
+  Serial.println("Resetting to 0 position");
+  digitalWrite(dirPin,LOW);
+  while(digitalRead(proximitySensorPin) != 0){
+    digitalWrite(stepPin,HIGH);
+    delay(5);
+    digitalWrite(stepPin,LOW);
+    delay(5);
   }
-  stepsPos += 1; // not sure if this is correct
+  for(int x = 0; x < 100; x++){ //move another 50 steps past the sensor
+      digitalWrite(stepPin,HIGH);
+      delay(5);
+      digitalWrite(stepPin,LOW);
+      delay(5);
+    }
+  stepsPos = 0;
+}
+
+void left(int steps) {
+  if (stepsPos+steps < maxRotation){//no problem, move as normal
+    digitalWrite(dirPin,HIGH); // HIGH = left (counterclockwise), LOW = right (clockwise) with black towards power of the motor
+    for(int x = 0; x < steps; x++){
+      digitalWrite(stepPin,HIGH);
+      delay(5);
+      digitalWrite(stepPin,LOW);
+      delay(5);
+    }
+    stepsPos += steps;
+  }else{//move back to 0 position before doing anything else
+    Serial.println("Move back to 0 position before doing anything else");
+    resetTo0();
+  }
+}
+
+void right(int steps) {
+  if (stepsPos-steps > 0){//no problem, move as normal
+    digitalWrite(dirPin,LOW); // HIGH = left (counterclockwise), LOW = right (clockwise) with black towards power of the motor
+    for(int x = 0; x < steps; x++){
+      digitalWrite(stepPin,HIGH);
+      delay(5);
+      digitalWrite(stepPin,LOW);
+      delay(5);
+    }
+    stepsPos -= steps;
+  }else{
+    Serial.println("Move to max position before doing anything else");
+    digitalWrite(dirPin,HIGH);
+    for(int x = 0; x < maxRotation; x++){
+      digitalWrite(stepPin,HIGH);
+      delay(5);
+      digitalWrite(stepPin,LOW);
+      delay(5);
+    }
+    stepsPos = maxRotation;
+  }
 }
