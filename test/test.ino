72a440a3102adb430b37f1bf37c9c776beed509a
tmarecic <tin.marecic@gmail.com>
2022-05-26 12:35:12 +0200

    Added fritzing wiring scheme and made file Main.ino

--- test/test.ino
+++ test/test.ino
@@ -9,6 +9,7 @@ int servoPos = 0;
 int stepsPos = 0;
 bool zeroPosTouched = false;
 String debug = "Did not touch";
+//From the front photoresistors
 const int pRTopRightPin = A1; // Photoresistor at Arduino analog pin A0 BLUE - top right
 const int pRTopLeftPin = A0; // Photoresistor at Arduino analog pin A1 GREEN - top left
 const int pRBottomRightPin = A3; // Photoresistor at Arduino analog pin A0 GREY - bottom right
@@ -17,12 +18,11 @@ int topRightResVal;
 int topLeftResVal;
 int bottomRightResVal;
 int bottomLeftResVal;
-int photoOffset = 3;
+int photoOffset = 5;
 Servo myservo;
 int servoStepSize = 1;
 int servoMaxPos = 45;
 int servoCurPos = 0;
-int divider = 5;
 
 
 
@@ -34,61 +34,38 @@ void setup() {
   pinMode(dirPin, OUTPUT);
   pinMode(stepPin, OUTPUT);
   digitalWrite(dirPin, HIGH);
-  Serial.println("Moving 120 steps to make sure the startup havent moved the motor too far");
-  for (int x = 0; x < 120; x++) {
-    myservo.write(0);
-    digitalWrite(dirPin, HIGH); // HIGH = left (counterclockwise), LOW = right (clockwise) with black towards power of the motor
-    digitalWrite(stepPin, HIGH);
-    delay(5);
-    digitalWrite(stepPin, LOW);
-    delay(5);
-  }
-  Serial.println("First steps done");
-  delay(2000);
-//  // Test if the motor is already above the sensor, if so move it away and back to get the correct 0 position
-  if (digitalRead(proximitySensorPin) == 0) {
-    Serial.println("Touched before moving, correting position");
-    digitalWrite(dirPin, HIGH); //set direction and move it until it no longer touches the sensor
-    Serial.println(proximitySensorPin);
-    while (digitalRead(proximitySensorPin) == 0) {
-      digitalWrite(stepPin, HIGH);
-      delay(5);
-      digitalWrite(stepPin, LOW);
-      delay(5);
-    }
-    for (int x = 0; x < 10; x++) { // move it 10 steps further to make sure its away from the sensor
-      digitalWrite(stepPin, HIGH);
-      delay(5);
-      digitalWrite(stepPin, LOW);
-      delay(5);
-    } // continue as if it never touched the sensor during setup
-  }
-  Serial.println("Checked if Touched done");
-  delay(2000);
-  resetTo0();
-  Serial.println("Calibration done, waiting 5 seconds before starting");
-  delay(5000);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   int val = digitalRead(proximitySensorPin);
+  Serial.println(val);
   topRightResVal = 0;
   topLeftResVal = 0;
   bottomRightResVal = 0;
   bottomLeftResVal = 0;
-  for (int i = 0; i < divider ; i ++) {
+  int divider = 10;
+  for (int i = 0; i < divider; i++) {
     topRightResVal += analogRead(pRTopRightPin);
-    topLeftResVal += analogRead(pRTopLeftPin) - 3;
+    topLeftResVal += analogRead(pRTopLeftPin);
     bottomRightResVal += analogRead(pRBottomRightPin);
-    bottomLeftResVal += analogRead(pRBottomLeftPin) - 3;
-    delay(50);
+    bottomLeftResVal += analogRead(pRBottomLeftPin);
+    delay(100);
   }
-  topRightResVal = topRightResVal / divider;
-  topLeftResVal = topLeftResVal / divider;
-  bottomRightResVal = bottomRightResVal / divider;
-  bottomLeftResVal = bottomLeftResVal / divider;
-
+  topRightResVal = topRightResVal/divider;
+  topLeftResVal = topLeftResVal/divider;
+  bottomRightResVal = bottomRightResVal/divider;
+  bottomLeftResVal = bottomLeftResVal/divider;
+  Serial.println("\n----------");
+  Serial.print("Top right:");
+  Serial.println(topRightResVal);
+  Serial.print("Top left:");
+  Serial.println(topLeftResVal);
+  Serial.print("Bottom right:");
+  Serial.println(bottomRightResVal);
+  Serial.print("Bottom left:");
+  Serial.println(bottomLeftResVal);
   int bottomVal = bottomLeftResVal + bottomRightResVal;
   int topVal = topLeftResVal + topRightResVal;
   int rightVal = topRightResVal + bottomRightResVal;
@@ -96,46 +73,52 @@ void loop() {
 
   if (bottomVal + (photoOffset * 3) < topVal) {
     //move up
-    up();
+    Serial.println("Moving up");
+    //up();
   }
-  else if (topVal + (photoOffset * 3) < bottomVal && ((servoCurPos - servoStepSize) > 0)) {
+  else if (topVal + (photoOffset * 3) < bottomVal && (servoCurPos - servoStepSize < 0)) {
     //move down
-    down();
+    Serial.println("Moving down");
+    //down();
 
   }
   else if (leftVal + (photoOffset * 3) < rightVal) {
     //move right
-    right(5);
+    Serial.println("Moving  right");
+    //right(5);
   }
   else if (rightVal + (photoOffset * 3) < leftVal) {
     //move left
-    left(5);
+    Serial.println("Moving left");
+    ///left(5);
   }
   else {
-    if (topLeftResVal > (topRightResVal + photoOffset) || bottomLeftResVal > (bottomRightResVal + photoOffset)) {
+    if (topLeftResVal > (topRightResVal + photoOffset) && bottomLeftResVal > (bottomRightResVal + photoOffset)) {
       //move to the left
-      left(5);
-    } else if (topRightResVal > (topLeftResVal + photoOffset) || bottomRightResVal > (bottomLeftResVal + photoOffset)) {
+      Serial.println("Moving left");
+      //left(5);
+    } else if (topRightResVal > (topLeftResVal + photoOffset) && bottomRightResVal > (bottomLeftResVal + photoOffset)) {
+      Serial.println("Moving right");
       //move to the right
-      right(5);
+      //right(5);
     }
-    if (topLeftResVal > (bottomLeftResVal + photoOffset) || topRightResVal > (bottomRightResVal + photoOffset)) {
+    if (topLeftResVal > (bottomLeftResVal + photoOffset) && topRightResVal > (bottomRightResVal + photoOffset)) {
       //move up
-      up();
-    } else if (bottomLeftResVal > (topLeftResVal + photoOffset) || bottomRightResVal > (topRightResVal + photoOffset)) {
+      Serial.println("Moving up");
+      //moveUp();
+    } else if (bottomLeftResVal > (topLeftResVal + photoOffset) && bottomRightResVal > (topRightResVal + photoOffset)) {
       //move down
-      down();
+      Serial.println("Moving down");
+      //moveDown();
     }
   }
-  delay(500);
+  delay(1000);
 }
 
 void resetTo0() {
   Serial.println("Resetting to 0 position");
   digitalWrite(dirPin, LOW);
-  int proxSensVal = digitalRead(proximitySensorPin);
-  Serial.println(proxSensVal);
-  while (proxSensVal != 0) {
+  while (digitalRead(proximitySensorPin) != 0) {
     Serial.println("Not touched");
     digitalWrite(stepPin, HIGH);
     delay(5);
